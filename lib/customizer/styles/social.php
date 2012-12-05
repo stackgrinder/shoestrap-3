b04d57ca4e5b7d179fd629bf825e92d13c198f82
Aristeides Stathopoulos <aristath@gmail.com>
2012-12-05 08:26:34 +0200

    Adding transient caching for social buttons styles

--- lib/customizer/styles/social.php
+++ lib/customizer/styles/social.php
@@ -24,126 +24,50 @@ function shoestrap_social_share_styles() {
   // if no color has been selected, set to #0066cc. This prevents errors with the php-less compiler.
   if ( strlen( $btn_color ) < 3 ) {
     $btn_color = '#0066cc';
-  } ?>
-
-  <style type="text/css">
-    .sharrre .box:hover{
-      padding-right:<?php echo ( $networks_nr * 30 + 40); ?>px;
-    }
-    .sharrre .middle a:hover{
-      text-decoration:none;
-    }
-    .sharrre .box:hover .middle{
-      width:<?php echo ( $networks_nr * 30 ); ?>px;
-    }
-    <?php if ( shoestrap_get_brightness( $btn_color ) >= 160 ) { ?>
-      .sharrre, .sharrre .middle a{color: #333;}
-      .sharrre .middle{
-        background: <?php echo shoestrap_adjust_brightness( $btn_color, -10 ); ?>;
-      }
-      .sharrre .right{
-        background: <?php echo shoestrap_adjust_brightness( $btn_color, -150 ); ?>;
-        color: <?php echo shoestrap_adjust_brightness( $btn_color, 20 ); ?>;
-      }
-    <?php } else { ?>
-      .sharrre, .sharrre .middle a{color: #fff;}
-      .sharrre .middle{
-        background: <?php echo shoestrap_adjust_brightness( $btn_color, 10 ); ?>;
-      }
-      .sharrre .right{
-        background: <?php echo shoestrap_adjust_brightness( $btn_color, 150 ); ?>;
-        color: <?php echo shoestrap_adjust_brightness( $btn_color, -20 ); ?>;
-      }
-    <?php } ?>
-    <?php
-    if ( class_exists( 'lessc' ) ) {
-      $less = new lessc;
-      
-      $less->setVariables( array(
-          "btnColor"  => $btn_color,
-      ));
-      $less->setFormatter( "compressed" );
-      
-      if ( shoestrap_get_brightness( $btn_color ) <= 160 ) {
-        // The code below is a copied from bootstrap's buttons.less + mixins.less files
-        echo $less->compile("
-          @btnColorHighlight: darken(spin(@btnColor, 5%), 10%);
+  }
   
-          .gradientBar(@primaryColor, @secondaryColor, @textColor: #fff, @textShadow: 0 -1px 0 rgba(0,0,0,.25)) {
-            color: @textColor;
-            text-shadow: @textShadow;
-            #gradient > .vertical(@primaryColor, @secondaryColor);
-            border-color: @secondaryColor @secondaryColor darken(@secondaryColor, 15%);
-            border-color: rgba(0,0,0,.1) rgba(0,0,0,.1) fadein(rgba(0,0,0,.1), 15%);
-          }
+  $styles = '<style type="text/css">';
+  $styles .= '.sharrre .box:hover{';
+  $styles .= 'padding-right:' . ( $networks_nr * 30 + 40 ) . 'px;}';
+  $styles .= '.sharrre .middle a:hover{text-decoration:none;}';
+  $styles .= '.sharrre .box:hover .middle{width:' . ( $networks_nr * 30 ) . 'px;}';
+  if ( shoestrap_get_brightness( $btn_color ) >= 160 ) {
+    $styles .= '.sharrre, .sharrre .middle a{color: #333;}';
+    $styles .= '.sharrre .middle{background:' . shoestrap_adjust_brightness( $btn_color, -10 ) . ';}';
+    $styles .= '.sharrre .right{background:' . shoestrap_adjust_brightness( $btn_color, -150 ) . ';';
+    $styles .= 'color: ' . shoestrap_adjust_brightness( $btn_color, 20 ) . ';}';
+  } else {
+    $styles .= '.sharrre, .sharrre .middle a{color: #fff;}';
+    $styles .= '.sharrre .middle{background:' . shoestrap_adjust_brightness( $btn_color, 10 ) . ';}';
+    $styles .= '.sharrre .right{background:' . shoestrap_adjust_brightness( $btn_color, 150 ) . ';';
+    $styles .= 'color:' . shoestrap_adjust_brightness( $btn_color, -20 ) . ';}';
+  }
+  $styles .= '.sharrre .box{background:' . shoestrap_adjust_brightness( $btn_color, -20 ) . '}';
+  $styles .= '</style>';
   
-          #gradient {
-            .vertical(@startColor: #555, @endColor: #333) {
-              background-color: mix(@startColor, @endColor, 60%);
-              background-image: -moz-linear-gradient(top, @startColor, @endColor); // FF 3.6+
-              background-image: -webkit-gradient(linear, 0 0, 0 100%, from(@startColor), to(@endColor)); // Safari 4+, Chrome 2+
-              background-image: -webkit-linear-gradient(top, @startColor, @endColor); // Safari 5.1+, Chrome 10+
-              background-image: -o-linear-gradient(top, @startColor, @endColor); // Opera 11.10
-              background-image: linear-gradient(to bottom, @startColor, @endColor); // Standard, IE10
-              background-repeat: repeat-x;
-            }
-          }
-  
-          .buttonBackground(@startColor, @endColor, @textColor: #fff, @textShadow: 0 -1px 0 rgba(0,0,0,.25)) {
-            .gradientBar(@startColor, @endColor, @textColor, @textShadow);
-            *background-color: @endColor; /* Darken IE7 buttons by default so they stand out more given they won't have borders */
-            .reset-filter();
-            &:hover, &:active, &.active, &.disabled, &[disabled] {
-              color: @textColor;
-              background-color: @endColor;
-              *background-color: darken(@endColor, 5%);
-            }
-          }
-          .sharrre .box{
-            .buttonBackground(@btnColor, @btnColorHighlight);
-          }
-        ");
-      } else {
-        echo $less->compile("
-          @btnColorHighlight: darken(@btnColor, 15%);
-  
-          .gradientBar(@primaryColor, @secondaryColor, @textColor: #333, @textShadow: 0 -1px 0 rgba(0,0,0,.25)) {
-            color: @textColor;
-            text-shadow: @textShadow;
-            #gradient > .vertical(@primaryColor, @secondaryColor);
-            border-color: @secondaryColor @secondaryColor darken(@secondaryColor, 15%);
-            border-color: rgba(0,0,0,.1) rgba(0,0,0,.1) fadein(rgba(0,0,0,.1), 15%);
-          }
-  
-          #gradient {
-            .vertical(@startColor: #555, @endColor: #333) {
-              background-color: mix(@startColor, @endColor, 60%);
-              background-image: -moz-linear-gradient(top, @startColor, @endColor); // FF 3.6+
-              background-image: -webkit-gradient(linear, 0 0, 0 100%, from(@startColor), to(@endColor)); // Safari 4+, Chrome 2+
-              background-image: -webkit-linear-gradient(top, @startColor, @endColor); // Safari 5.1+, Chrome 10+
-              background-image: -o-linear-gradient(top, @startColor, @endColor); // Opera 11.10
-              background-image: linear-gradient(to bottom, @startColor, @endColor); // Standard, IE10
-              background-repeat: repeat-x;
-            }
-          }
-  
-          .buttonBackground(@startColor, @endColor, @textColor: #333, @textShadow: 0 -1px 0 rgba(0,0,0,.25)) {
-            .gradientBar(@startColor, @endColor, @textColor, @textShadow);
-            *background-color: @endColor; /* Darken IE7 buttons by default so they stand out more given they won't have borders */
-            .reset-filter();
-            &:hover, &:active, &.active, &.disabled, &[disabled] {
-              color: @textColor;
-              background-color: @endColor;
-              *background-color: darken(@endColor, 5%);
-            }
-          }
-          .sharrre .box{
-            .buttonBackground(@btnColor, @btnColorHighlight);
-          }
-        ");
-      }
-    } ?>
-  </style>
-  <?php
+  return $styles;
 }
 add_action( 'wp_head', 'shoestrap_social_share_styles' );
+
+/*
+ * Set cache for 24 hours
+ */
+function shoestrap_social_share_styles_cache() {
+  $data = shoestrap_social_share_styles();
+  if ( $data === false ) {
+    $data = shoestrap_social_share_styles();
+    set_transient( 'shoestrap_social_share_styles', $data, 3600 * 24 );
+  }
+  echo $data;
+}
+add_action( 'wp_head', 'shoestrap_social_share_styles_cache', 199 );
+
+/*
+ * Reset cache when in customizer
+ */
+function shoestrap_social_share_styles_cache_reset() {
+  delete_transient( 'shoestrap_social_share_styles' );
+  shoestrap_social_share_styles_cache();
+}
+add_action( 'customize_preview_init', 'shoestrap_social_share_styles_cache_reset' );
+
