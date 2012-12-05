cc3fd93590b37d2c2b3f238e5b4bfa80675e3a66
Aristeides Stathopoulos <aristath@gmail.com>
2012-12-06 01:33:59 +0200

    simplifying the call to action button css and avoid using the less compiler

--- lib/customizer/styles/hero.php
+++ lib/customizer/styles/hero.php
@@ -53,95 +53,26 @@ function shoestrap_css_hero() {
     $textColor = '#333333';
   }
   
-    if ( class_exists( 'lessc' ) ) {
-      $less = new lessc;
-      
-      $less->setVariables( array(
-          "btnColor"  => $shoestrap_hero_cta_color,
-          "textColor" => $textColor
-      ));
-      $less->setFormatter( "compressed" );
-      
-      if ( shoestrap_get_brightness( $shoestrap_hero_cta_color ) <= 160) {
-        // The code below is a copied from bootstrap's buttons.less + mixins.less files
-        $styles .= $less->compile("
-          @btnColorHighlight: darken(spin(@btnColor, 5%), 10%);
+  $startColor = $shoestrap_hero_cta_color;
+  $endColor   = shoestrap_adjust_brightness( $startColor, -63 );
   
-          .gradientBar(@primaryColor, @secondaryColor, @textColor: #fff, @textShadow: 0 -1px 0 rgba(0,0,0,.25)) {
-            color: @textColor;
-            text-shadow: @textShadow;
-            #gradient > .vertical(@primaryColor, @secondaryColor);
-            border-color: @secondaryColor @secondaryColor darken(@secondaryColor, 15%);
-            border-color: rgba(0,0,0,.1) rgba(0,0,0,.1) fadein(rgba(0,0,0,.1), 15%);
-          }
-  
-          #gradient {
-            .vertical(@startColor: #555, @endColor: #333) {
-              background-color: mix(@startColor, @endColor, 60%);
-              background-image: -moz-linear-gradient(top, @startColor, @endColor); // FF 3.6+
-              background-image: -webkit-gradient(linear, 0 0, 0 100%, from(@startColor), to(@endColor)); // Safari 4+, Chrome 2+
-              background-image: -webkit-linear-gradient(top, @startColor, @endColor); // Safari 5.1+, Chrome 10+
-              background-image: -o-linear-gradient(top, @startColor, @endColor); // Opera 11.10
-              background-image: linear-gradient(to bottom, @startColor, @endColor); // Standard, IE10
-              background-repeat: repeat-x;
-            }
-          }
-  
-          .buttonBackground(@startColor, @endColor, @textColor: #fff, @textShadow: 0 -1px 0 rgba(0,0,0,.25)) {
-            .gradientBar(@startColor, @endColor, @textColor, @textShadow);
-            *background-color: @endColor; /* Darken IE7 buttons by default so they stand out more given they won't have borders */
-            .reset-filter();
-            &:hover, &:active, &.active, &.disabled, &[disabled] {
-              color: @textColor;
-              background-color: @endColor;
-              *background-color: darken(@endColor, 5%);
-            }
-          }
-          .jumbotron .btn{
-            .buttonBackground(@btnColor, @btnColorHighlight);
-          }
-        ");
-      } else {
-        $styles .= $less->compile("
-          @btnColorHighlight: darken(@btnColor, 15%);
-  
-          .gradientBar(@primaryColor, @secondaryColor, @textColor: #333, @textShadow: 0 -1px 0 rgba(0,0,0,.25)) {
-            color: @textColor;
-            text-shadow: @textShadow;
-            #gradient > .vertical(@primaryColor, @secondaryColor);
-            border-color: @secondaryColor @secondaryColor darken(@secondaryColor, 15%);
-            border-color: rgba(0,0,0,.1) rgba(0,0,0,.1) fadein(rgba(0,0,0,.1), 15%);
-          }
-  
-          #gradient {
-            .vertical(@startColor: #555, @endColor: #333) {
-              background-color: mix(@startColor, @endColor, 60%);
-              background-image: -moz-linear-gradient(top, @startColor, @endColor); // FF 3.6+
-              background-image: -webkit-gradient(linear, 0 0, 0 100%, from(@startColor), to(@endColor)); // Safari 4+, Chrome 2+
-              background-image: -webkit-linear-gradient(top, @startColor, @endColor); // Safari 5.1+, Chrome 10+
-              background-image: -o-linear-gradient(top, @startColor, @endColor); // Opera 11.10
-              background-image: linear-gradient(to bottom, @startColor, @endColor); // Standard, IE10
-              background-repeat: repeat-x;
-            }
-          }
-  
-          .buttonBackground(@startColor, @endColor, @textColor: #333, @textShadow: 0 -1px 0 rgba(0,0,0,.25)) {
-            .gradientBar(@startColor, @endColor, @textColor, @textShadow);
-            *background-color: @endColor; /* Darken IE7 buttons by default so they stand out more given they won't have borders */
-            .reset-filter();
-            &:hover, &:active, &.active, &.disabled, &[disabled] {
-              color: @textColor;
-              background-color: @endColor;
-              *background-color: darken(@endColor, 5%);
-            }
-          }
-          .jumbotron .btn{
-            .buttonBackground(@btnColor, @btnColorHighlight);
-          }
-        ");
-      }
-    }
-    $styles .= '</style>';
+  $styles = '<style>';
+  $styles .= '.jumbotron .btn{';
+  $styles .= 'color: ' . $textColor . ';';
+  $styles .= 'background-color: ' . shoestrap_mix_colors( $startColor, $endColor, 60 ) . ';';
+  $styles .= 'background-image: -moz-linear-gradient(top, ' . $startColor . ', ' . $endColor . ');';
+  $styles .= 'background-image: -webkit-gradient(linear, 0 0, 0 100%, from(' . $startColor . '), to(' . $endColor . '));';
+  $styles .= 'background-image: -webkit-linear-gradient(top, ' . $startColor . ', ' . $endColor . ');';
+  $styles .= 'background-image: -o-linear-gradient(top, ' . $startColor . ', ' . $endColor . ');';
+  $styles .= 'background-image: linear-gradient(to bottom, ' . $startColor . ', ' . $endColor . ');';
+  $styles .= 'background-repeat: repeat-x;';
+  $styles .= '*background-color: ' . $endColor . ';}';
+  $styles .= '.jumbotron .btn:hover, .jumbotron .btn:active, .jumbotron .btn.active, .jumbotron .btn.disabled, .jumbotron .btn[disabled] {';
+  $styles .= 'color: ' . $textColor . ';';
+  $styles .= 'background-color: ' . $endColor . ';';
+  $styles .= '*background-color: ' . shoestrap_adjust_brightness( $endColor, -12 ) . ';}';
+  $styles .= '</style>';
+
   return $styles;
 }
 
