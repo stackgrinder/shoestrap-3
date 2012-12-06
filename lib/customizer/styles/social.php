6d271b93cc109fdd4c5bf71e70f72efc5eb68dc8
Aristeides Stathopoulos <aristath@gmail.com>
2012-12-06 04:14:23 +0200

    better social buttons styling

--- lib/customizer/styles/social.php
+++ lib/customizer/styles/social.php
@@ -4,6 +4,7 @@
  * Apply any css needed for the social sharing buttons.
  */
 function shoestrap_social_share_styles() {
+  $background   = get_theme_mod( 'shoestrap_background_color' );
   $googleplus   = get_theme_mod( 'shoestrap_gplus_on_posts' );
   $facebook     = get_theme_mod( 'shoestrap_facebook_on_posts' );
   $twitter      = get_theme_mod( 'shoestrap_twitter_on_posts' );
@@ -25,7 +26,16 @@ function shoestrap_social_share_styles() {
   if ( strlen( $btn_color ) < 3 ) {
     $btn_color = '#0066cc';
   }
+
+  if ( get_theme_mod( 'shoestrap_flat_buttons' ) == 1 ) {
+    $btnColorHighlight = $btn_color;
+  } else {
+    $btnColorHighlight = shoestrap_adjust_brightness( $btn_color, -63 );
+  }
   
+  $startColor = $btn_color;
+  $endColor   = $btnColorHighlight;
+
   $styles = '<style type="text/css">';
   $styles .= '.sharrre .box:hover{';
   $styles .= 'padding-right:' . ( $networks_nr * 30 + 40 ) . 'px;}';
@@ -34,15 +44,23 @@ function shoestrap_social_share_styles() {
   if ( shoestrap_get_brightness( $btn_color ) >= 160 ) {
     $styles .= '.sharrre, .sharrre .middle a{color: #333;}';
     $styles .= '.sharrre .middle{background:' . shoestrap_adjust_brightness( $btn_color, -10 ) . ';}';
-    $styles .= '.sharrre .right{background:' . shoestrap_adjust_brightness( $btn_color, -150 ) . ';';
-    $styles .= 'color: ' . shoestrap_adjust_brightness( $btn_color, 20 ) . ';}';
+    $styles .= '.sharrre .right{background:' . $background . ';';
+    $styles .= 'color: ' . shoestrap_adjust_brightness( $btn_color, -60 ) . ';}';
   } else {
     $styles .= '.sharrre, .sharrre .middle a{color: #fff;}';
     $styles .= '.sharrre .middle{background:' . shoestrap_adjust_brightness( $btn_color, 10 ) . ';}';
-    $styles .= '.sharrre .right{background:' . shoestrap_adjust_brightness( $btn_color, 150 ) . ';';
-    $styles .= 'color:' . shoestrap_adjust_brightness( $btn_color, -20 ) . ';}';
+    $styles .= '.sharrre .right{background:' . $background . ';';
+    $styles .= 'color:' . shoestrap_adjust_brightness( $btn_color, 60 ) . ';}';
   }
-  $styles .= '.sharrre .box{background:' . shoestrap_adjust_brightness( $btn_color, -20 ) . '}';
+  $styles .= '.sharrre .box{';
+  $styles .= 'background: ' . shoestrap_mix_colors( $startColor, $endColor, 60 ) . ';';
+  $styles .= 'background-image: -moz-linear-gradient(top, ' . $startColor . ', ' . $endColor . ');';
+  $styles .= 'background-image: -webkit-gradient(linear, 0 0, 0 100%, from(' . $startColor . '), to(' . $endColor . '));';
+  $styles .= 'background-image: -webkit-linear-gradient(top, ' . $startColor . ', ' . $endColor . ');';
+  $styles .= 'background-image: -o-linear-gradient(top, ' . $startColor . ', ' . $endColor . ');';
+  $styles .= 'background-image: linear-gradient(to bottom, ' . $startColor . ', ' . $endColor . ');';
+  $styles .= 'background-repeat: repeat-x;';
+  $styles .= '*background-color: ' . $endColor . ';}';
   $styles .= '</style>';
   
   return $styles;
