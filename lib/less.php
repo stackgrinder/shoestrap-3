50332846f4ac421692343550a4a45b570f42fe5a
Aristeides Stathopoulos <aristath@gmail.com>
2012-12-18 17:01:16 +0200

    only load the php compiler when advanced builder is ON

--- lib/less.php
+++ lib/less.php
@@ -3,33 +3,36 @@
 function shoestrap_phpless(){
   
   $shoestrap_responsive = get_theme_mod( 'shoestrap_responsive' );
+  $advanced_builder = get_option('shoestrap_advanced_compiler');
   
-  if ( !class_exists( 'lessc' ) ) {
-    require_once locate_template( '/lib/less_compiler/lessc.inc.php' );
-  }
-  $less = new lessc;
-  // $less->setFormatter( "compressed" );
+  if ( $advanced_builder == 1 ) {
+    if ( !class_exists( 'lessc' ) ) {
+      require_once locate_template( '/lib/less_compiler/lessc.inc.php' );
+    }
+    $less = new lessc;
+    // $less->setFormatter( "compressed" );
+    
+    if ( $shoestrap_responsive == '0' ) {
+      $inputFile  = locate_template( 'assets/css/app-fixed.less' );
+      $outputFile = locate_template( 'assets/css/app-fixed.css' );
+    } else {
+      $inputFile  = locate_template( 'assets/css/app-responsive.less' );
+      $outputFile = locate_template( 'assets/css/app-responsive.css' );
+    }
   
-  if ( $shoestrap_responsive == '0' ) {
-    $inputFile  = locate_template( 'assets/css/app-fixed.less' );
-    $outputFile = locate_template( 'assets/css/app-fixed.css' );
-  } else {
-    $inputFile  = locate_template( 'assets/css/app-responsive.less' );
-    $outputFile = locate_template( 'assets/css/app-responsive.css' );
-  }
-
-  $less = new lessc;
-
-  // create a new cache object, and compile
-  $cache = $less -> cachedCompile( $inputFile );
-
-  file_put_contents( $outputFile, $cache["compiled"] );
-
-  // the next time we run, write only if it has updated
-  $last_updated = $cache['updated'];
-  $cache = $less -> cachedCompile( $cache );
-  if ( $cache['updated'] > $last_updated ) {
-    file_put_contents( $outputFile, $cache['compiled'] );
+    $less = new lessc;
+  
+    // create a new cache object, and compile
+    $cache = $less -> cachedCompile( $inputFile );
+  
+    file_put_contents( $outputFile, $cache["compiled"] );
+  
+    // the next time we run, write only if it has updated
+    $last_updated = $cache['updated'];
+    $cache = $less -> cachedCompile( $cache );
+    if ( $cache['updated'] > $last_updated ) {
+      file_put_contents( $outputFile, $cache['compiled'] );
+    }
   }
 }
 add_action('wp', 'shoestrap_phpless');
