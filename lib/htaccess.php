593dcb98cf5366f0c645ad89b373c5647856cea3
Aristeides Stathopoulos <aristath@gmail.com>
2012-12-18 19:30:23 +0200

    fixes #44

--- lib/htaccess.php
+++ lib/htaccess.php
@@ -36,6 +36,7 @@ if (stristr($_SERVER['SERVER_SOFTWARE'], 'apache') || stristr($_SERVER['SERVER_S
       'assets/css/(.*)'      => THEME_PATH . '/assets/css/$1',
       'assets/js/(.*)'       => THEME_PATH . '/assets/js/$1',
       'assets/img/(.*)'      => THEME_PATH . '/assets/img/$1',
+      'assets/fonts/(.*)'    => THEME_PATH . '/assets/fonts/$1',
       'plugins/(.*)'  => RELATIVE_PLUGIN_PATH . '/$1'
     );
     $wp_rewrite->non_wp_rules = array_merge($wp_rewrite->non_wp_rules, $shoestrap_new_non_wp_rules);
