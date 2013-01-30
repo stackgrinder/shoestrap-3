6a35db2e646e359f091284f908016e3dc7aeaf8d
Aristeides Stathopoulos <aristath@gmail.com>
2013-01-30 13:08:44 +0200

    added some comments

--- lib/customizer/styles/navbar.php
+++ lib/customizer/styles/navbar.php
@@ -4,6 +4,7 @@
  * Applies the styles to the navbar.
  */
 function shoestrap_navbar_css(){
+  // Get the customizer settings
   $header_bg_color      = get_theme_mod( 'shoestrap_header_backgroundcolor' );
   $navbar_color         = get_theme_mod( 'shoestrap_navbar_color' );
   $navbar_textcolor     = get_theme_mod( 'shoestrap_navbar_textcolor' );
@@ -16,14 +17,18 @@ function shoestrap_navbar_css(){
   $navbar_color     = '#' . str_replace( '#', '', $navbar_color );
   $navbar_textcolor = '#' . str_replace( '#', '', $navbar_textcolor );
   
+  // Make sure the padding is properly formatted
   if ( $navbar_logo_padding >= 1 ) {} else { $navbar_logo_padding = 0; }
   
+  // Apply proper padding for logos
   $styles = '<style>';
   if ( get_theme_mod( 'shoestrap_logo' ) ) {
     if ( get_theme_mod( 'shoestrap_header_mode' ) == 'navbar' ) {
       '.navbar a.brand{padding: 5px 20px 5px;}';
     }
   }
+  
+  // Navbar colors
   $styles .= '.navbar-inner, #main-subnav.subnav-fixed, .navbar-inner .dropdown-menu{';
   $styles .= 'background-color:' . $navbar_color . '!important;';
   if ( $navbar_no_gradient != 1 ) {
@@ -43,16 +48,21 @@ function shoestrap_navbar_css(){
     $styles .= 'filter: e(%("progid:DXImageTransform.Microsoft.gradient(startColorstr="%d", endColorstr="%d", GradientType=0)",argb(' . $navbar_color . '),argb(' . $navbar_color . '))) !important;';
     $styles .= 'border: 1px solid ' . shoestrap_adjust_brightness( $navbar_color, -20 ) . ';';
   }
+
+  // Navbar Padding
   $styles .= 'padding:' . $navbar_logo_padding . 'px;}';
   
+  // Navbar Dropdown colors
   $styles .= '.navbar-inner .dropdown-menu{padding: 0;}';
-
+  
+  // Extra css for non-fixed navbar
   if ( $navbar_fixed != 1 ) {
     $styles .= '.navbar.navbar-fixed-top{position:relative;}';
     $styles .= 'body.admin-bar .navbar-fixed-top{top:0;}';
     $styles .= 'body.top-navbar{padding-top:0;}';
   }
   
+  // Navbar Button colors
   $styles .= '.btn.btn-navbar{';
   if ( shoestrap_get_brightness( $navbar_color ) >= 160 ) {
     $styles .= 'background: ' . shoestrap_adjust_brightness( $navbar_color, -40 ) . ';';
@@ -69,6 +79,7 @@ function shoestrap_navbar_css(){
   }
   $styles .= '}';
   
+  // Navbar menu items text-color and active menu styling
   $styles .= '.navbar-inner a, .navbar-inner .brand, .navbar .nav > li > a{';
   if ( strlen( $navbar_textcolor ) < 6 ) {
     if ( shoestrap_get_brightness( $navbar_color ) >= 160 ) {
@@ -114,7 +125,8 @@ function shoestrap_navbar_css(){
     }
   }
   $styles .= 'text-shadow: 0 1px 0 ' . shoestrap_adjust_brightness( $navbar_color, -15 ) . ';}';
-
+  
+  // Destroy active menu item background when user has added padding
   if ( $navbar_logo_padding >= 1 ) {
     $styles .= '.navbar .nav > .active > a, .navbar .nav > .active > a:hover, .navbar .nav > .active > a:focus{';
     $styles .= 'background-color:none; background-color:transparent;';
