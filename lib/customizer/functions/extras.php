f1cc64c6c2678eae765a83e187dc921118668e14
Aristeides Stathopoulos <aristath@gmail.com>
2013-02-05 13:33:08 +0200

    cleaning up

--- lib/customizer/functions/extras.php
+++ lib/customizer/functions/extras.php
@@ -7,13 +7,3 @@ function shoestrap_remove_controls( $wp_customize ){
   $wp_customize->remove_control( 'header_textcolor');
 }
 add_action( 'customize_register', 'shoestrap_remove_controls' );
-
-/**
- * Bind JS handlers to make Theme Customizer preview reload changes asynchronously.
- *
- */
-function shoestrap_customize_preview( $wp_customize ) {
-  wp_enqueue_script( 'shoestrap-customizer', get_stylesheet_directory_uri() . '/lib/customizer/js/theme-customizer.js', array( 'customize-preview' ) );
-}
-add_action( 'customize_preview_init', 'shoestrap_customize_preview' );
-
