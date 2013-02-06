58c33f0b49c155459465107e58cee88f057e91e2
Aristeides Stathopoulos <aristath@gmail.com>
2013-02-06 10:19:15 +0200

    moving header template

--- lib/customizer/header/functions.php
+++ lib/customizer/header/functions.php
@@ -78,3 +78,21 @@ function shoestrap_header_customizer( $wp_customize ){
   
 }
 add_action( 'customize_register', 'shoestrap_header_customize' );
+
+function shoestrap_branding() {
+  if ( get_theme_mod( 'shoestrap_extra_branding' ) == 1 ) { ?>
+    <div class="container-fluid logo-wrapper">
+      <div class="logo container">
+        <div class="row-fluid">
+          <div class="span8">
+            <a class="brand-logo" href="<?php echo home_url(); ?>/">
+              <h1><?php if ( function_exists( 'shoestrap_logo' ) ) { shoestrap_logo(); } ?></h1>
+            </a>
+          </div>
+          <?php if ( has_action( 'shoestrap_branding_branding_right' ) ) { do_action( 'shoestrap_branding_branding_right' ); } ?>
+        </div>
+      </div>
+    </div>
+  <?php }
+}
+add_action( 'shoestrap_branding', 'shoestrap_branding' );
