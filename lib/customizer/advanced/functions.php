f860886eb74108c3bc1b0447bdb6af6824e78d6a
Aristeides Stathopoulos <aristath@gmail.com>
2013-02-06 10:04:25 +0200

    advanced settings

--- lib/customizer/advanced/functions.php
+++ lib/customizer/advanced/functions.php
@@ -8,5 +8,13 @@ function shoestrap_register_sections( $wp_customize ){
   foreach( $sections as $section ){
     $wp_customize->add_section( $section['slug'], array( 'title' => $section['title'], 'priority' => $section['priority'] ) );
   }
+
+  $settings   = array();
+  $settings[] = array( 'slug' => 'shoestrap_advanced_head',             'default' => '' );
+  $settings[] = array( 'slug' => 'shoestrap_advanced_footer',           'default' => '' );
+
+  foreach( $settings as $setting ){
+    $wp_customize->add_setting( $setting['slug'], array( 'default' => $setting['default'], 'type' => 'theme_mod', 'capability' => 'edit_theme_options' ) );
+  }
 }
 add_action( 'customize_register', 'shoestrap_register_sections' );
