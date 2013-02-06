fa82d9ff9dc3583d860c1e056635e84d448c94a9
Aristeides Stathopoulos <aristath@gmail.com>
2013-02-06 12:12:15 +0200

    renaming function from roots_ to shoestrap_

--- lib/cleanup.php
+++ lib/cleanup.php
@@ -509,7 +509,7 @@ add_filter('request', 'shoestrap_request_filter');
 /**
  * Tell WordPress to use searchform.php from the templates/ directory
  */
-function roots_get_search_form() {
+function shoestrap_get_search_form() {
   locate_template('/templates/searchform.php', true, true);
 }
 add_filter('get_search_form', 'shoestrap_get_search_form');
