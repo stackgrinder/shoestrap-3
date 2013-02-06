432d04c4915a3d290b1bdabd9b9916ce2ed14102
Aristeides Stathopoulos <aristath@gmail.com>
2013-02-06 10:01:27 +0200

    advanced section

--- lib/customizer/advanced/functions.php
+++ lib/customizer/advanced/functions.php
@@ -1,12 +1,9 @@
 <?php
 
-/*
- * Adds sections to the customizer
- */
 function shoestrap_register_sections( $wp_customize ){
   
   $sections   = array();
-  $sections[] = array( 'slug' => 'shoestrap_general',           'title' => __( 'Presets', 'shoestrap' ),          'priority' => 15 );
+  $sections[] = array( 'slug' => 'shoestrap_advanced',          'title' => __( 'Advanced', 'shoestrap' ),         'priority' => 9 );
 
   foreach( $sections as $section ){
     $wp_customize->add_section( $section['slug'], array( 'title' => $section['title'], 'priority' => $section['priority'] ) );
