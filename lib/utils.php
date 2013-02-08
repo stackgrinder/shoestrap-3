2cb35b09d81346073eb0d64ab1faa8b807ccfca0
Aristeides Stathopoulos <aristath@gmail.com>
2013-02-08 20:20:44 +0200

    Output author title with get_the_author https://github.com/retlehs/roots/commit/4093095dc7dbfef8f82bce546ae90634689ac025

--- lib/utils.php
+++ lib/utils.php
@@ -26,9 +26,7 @@ function shoestrap_title() {
     } elseif (is_year()) {
       printf(__('Yearly Archives: %s', 'shoestrap'), get_the_date('Y'));
     } elseif (is_author()) {
-      global $post;
-      $author_id = $post->post_author;
-      printf(__('Author Archives: %s', 'shoestrap'), get_the_author_meta('display_name', $author_id));
+      printf(__('Author Archives: %s', 'roots'), get_the_author());
     } else {
       single_cat_title();
     }
