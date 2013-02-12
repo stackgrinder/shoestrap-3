17c0d53e47943558960b813f6b8572a788c08cf9
Aristeides Stathopoulos <aristath@gmail.com>
2013-02-12 18:20:01 +0200

    added the "shoestrap_general_no_gradients" setting

--- lib/customizer/general/functions.php
+++ lib/customizer/general/functions.php
@@ -11,5 +11,12 @@ function shoestrap_general_customizer( $wp_customize ){
   foreach( $sections as $section ){
     $wp_customize->add_section( $section['slug'], array( 'title' => $section['title'], 'priority' => $section['priority'] ) );
   }
+  
+  $settings   = array();
+  $settings[] = array( 'slug' => 'shoestrap_general_no_gradients', 'default' => '' );
+  
+  foreach( $settings as $setting ){
+    $wp_customize->add_setting( $setting['slug'], array( 'default' => $setting['default'], 'type' => 'theme_mod', 'capability' => 'edit_theme_options' ) );
+  }
 }
 add_action( 'customize_register', 'shoestrap_hero_customizer' );
