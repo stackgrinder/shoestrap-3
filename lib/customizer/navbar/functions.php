657f22efc2c31276311fb564d24e352598a7c16e
Aristeides Stathopoulos <aristath@gmail.com>
2013-02-12 17:54:40 +0200

    removing extraneous navbar comments

--- lib/customizer/navbar/functions.php
+++ lib/customizer/navbar/functions.php
@@ -30,7 +30,7 @@ function shoestrap_navbar_customizer( $wp_customize ){
   $settings[] = array( 'slug' => 'shoestrap_navbar_original_logo',      'default' => '' );
   $settings[] = array( 'slug' => 'shoestrap_navbar_no_gradient',        'default' => '' );
   $settings[] = array( 'slug' => 'shoestrap_navbar_fixed',              'default' => '' );
-  
+
   // Secondary NavBar Settings
   $settings[] = array( 'slug' => 'shoestrap_navbar_secondary',          'default' => '' );
   $settings[] = array( 'slug' => 'shoestrap_navbar2_loginlink',         'default' => '' );
@@ -60,41 +60,24 @@ function shoestrap_navbar_customizer( $wp_customize ){
   // Navbar text color
   $color_controls[] = array( 'setting' => 'shoestrap_navbar_textcolor',       'label' => 'Navbar Text Color',               'section' => 'shoestrap_primary_navbar',            'priority' => 40 );
   
-  /*
-   * Checkbox Controls
-   */
+  // Checkbox Controls
   $checkbox_controls = array();
-  // Display Navbar on top
   $checkbox_controls[] = array( 'setting' => 'shoestrap_navbar_top',          'label' => 'Display NavBar on the top of the page', 'section' => 'shoestrap_primary_navbar',  'priority' => 1 );
-  // Display Navbar Branding
   $checkbox_controls[] = array( 'setting' => 'shoestrap_navbar_branding',     'label' => 'Display Branding (Sitename or Logo)',   'section' => 'shoestrap_primary_navbar',  'priority' => 2 );
-  // Display NavBar Logo
   $checkbox_controls[] = array( 'setting' => 'shoestrap_navbar_logo',         'label' => 'Use Logo (if available) for branding',  'section' => 'shoestrap_primary_navbar',  'priority' => 3 );
-  // Display NavBar Social links
   $checkbox_controls[] = array( 'setting' => 'shoestrap_navbar_social',       'label' => 'Display Social Links in the Navbar',    'section' => 'shoestrap_primary_navbar',  'priority' => 6 );
-  // Display Primary NavBar Searchbox
   $checkbox_controls[] = array( 'setting' => 'shoestrap_p_navbar_searchbox',  'label' => 'Display Search',                        'section' => 'shoestrap_primary_navbar',  'priority' => 7 );
-  // NavBar Menu on the left/right selection
   $checkbox_controls[] = array( 'setting' => 'shoestrap_nav_pull',            'label' => 'Menu on the Right',                     'section' => 'shoestrap_primary_navbar',  'priority' => 15 );
-  // NavBar Logo original size
   $checkbox_controls[] = array( 'setting' => 'shoestrap_navbar_original_logo','label' => 'Original Logo Size',                    'section' => 'shoestrap_primary_navbar',  'priority' => 20 );
-  // No gradients on the NavBar
   $checkbox_controls[] = array( 'setting' => 'shoestrap_navbar_no_gradient',  'label' => 'Disable gradients',                     'section' => 'shoestrap_primary_navbar',  'priority' => 30 );
-  // No gradients on the NavBar
   $checkbox_controls[] = array( 'setting' => 'shoestrap_navbar_fixed',        'label' => 'Fixed Positioning',                     'section' => 'shoestrap_primary_navbar',  'priority' => 32 );
-  // Show/Hide the login link
   $checkbox_controls[] = array( 'setting' => 'shoestrap_header_loginlink',    'label' => 'Show Login/Logout Link',                'section' => 'shoestrap_primary_navbar',  'priority' => 5 );
-
-  // Display Secondary Navbar
   $checkbox_controls[] = array( 'setting' => 'shoestrap_navbar_secondary',    'label' => 'Display Secondary NavBar',              'section' => 'shoestrap_secondary_navbar','priority' => 1 );
-  // Show/Hide the login link on the secondary navbar
   $checkbox_controls[] = array( 'setting' => 'shoestrap_navbar2_loginlink',   'label' => 'Show Login/Logout Link',                'section' => 'shoestrap_secondary_navbar','priority' => 5 );
-  // Display NavBar Social links on the secondary navbar
   $checkbox_controls[] = array( 'setting' => 'shoestrap_navbar2_social',      'label' => 'Display Social Links in the Navbar',    'section' => 'shoestrap_secondary_navbar','priority' => 6 );
 
   // Text Controls
   $text_controls = array();
-  // Kigi padding
   $text_controls[]  = array( 'setting' => 'shoestrap_navbar_logo_padding',    'label' => 'Navbar Padding (px)',                   'section' => 'shoestrap_primary_navbar',  'priority' => 40 );
   
 
