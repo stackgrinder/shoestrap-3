20bc893a824c169973f578e92ae9c228e0d442b4
Aristeides Stathopoulos <aristath@gmail.com>
2013-02-25 19:07:24 +0200

    Remove disabled post nav links https://github.com/retlehs/roots/commit/6b19c3a5ae2777507bc61e75e7f2625ba3916194

--- templates/content.php
+++ templates/content.php
@@ -26,13 +26,9 @@
     <ul class="pager">
       <?php if (get_next_posts_link()) : ?>
         <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'shoestrap')); ?></li>
-      <?php else: ?>
-        <li class="previous disabled"><a><?php _e('&larr; Older posts', 'shoestrap'); ?></a></li>
       <?php endif; ?>
       <?php if (get_previous_posts_link()) : ?>
         <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'shoestrap')); ?></li>
-      <?php else: ?>
-        <li class="next disabled"><a><?php _e('Newer posts &rarr;', 'shoestrap'); ?></a></li>
       <?php endif; ?>
     </ul>
   </nav>
