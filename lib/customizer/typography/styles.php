4fc8352e26e771fb72b2026f83ce656391bd2115
Aristeides Stathopoulos <aristath@gmail.com>
2013-03-22 10:20:15 +0200

    Make sure webfonts work in advanced customizer as well

--- lib/customizer/typography/styles.php
+++ lib/customizer/typography/styles.php
@@ -50,20 +50,3 @@ function shoestrap_typography_css() {
   
   return $styles;
 }
-
-/*
- * Extract the name of the webfont and enqueue its style.
- */
-function shoestrap_typography_webfont() {
-  $webfont           = get_theme_mod( 'shoestrap_google_webfonts' );
-  $webfont_weight      = get_theme_mod( 'shoestrap_webfonts_weight' );
-  $webfont_character_set = get_theme_mod( 'shoestrap_webfonts_character_set' );
-  
-  $f       = strlen( $webfont );
-  if ($f > 3){
-    $webfontname = str_replace( ' ', '+', $webfont );
-    
-  return '<link href="http://fonts.googleapis.com/css?family=' . $webfontname . ':' . $webfont_weight . '&subset=' . $webfont_character_set . '" rel="stylesheet" type="text/css">';
-  
-  }
-}
