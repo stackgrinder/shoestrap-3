2fe913802dc3b85f02800ddfd3240a7d47b5aa7b
Aristeides Stathopoulos <aristath@gmail.com>
2013-04-16 20:00:15 +0300

    preliminary admin notices work

---
+++ lib/admin/notices.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * Check the files permissions and echo a message for admins
+ * if files are not writable.
+ */
+function shoestrap_check_files_permissions() {
+
+  $files  = array(
+    'variables'   =>  locate_template( 'assets/less/bootstrap/variables.less' ),
+    'app'         =>  locate_template( 'assets/css/app.css' ),
+    'appng'       =>  locate_template( 'assets/css/app-no-gradiens.css' ),
+    'appngnr'     =>  locate_template( 'assets/css/app-no-gradients-no-radius.css' ),
+    'appnr'       =>  locate_template( 'assets/css/app-no-radius.css' ),
+    'responsive'  =>  locate_template( 'assets/css/responsive.css' ),
+  );
+  
+  echo '<div class="error">';
+  foreach ( $files as $file ) {
+    echo '<p>';
+    echo $file . ' : ';
+    if ( is_writable( $file ) )
+      echo '<span style="color: #00aa00;">' .  __( 'Writable', 'shoestrap' );
+    else
+      echo '<span style="color: #aa0000;">' .  __( 'Not Writable', 'shoestrap' );
+    
+    echo '</p>';
+  }
+  echo '</div>';
+}
+add_action( 'admin_notices', 'shoestrap_check_files_permissions' );
