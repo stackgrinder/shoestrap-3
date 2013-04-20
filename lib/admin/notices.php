b6623d3e88c20dac040c780a709fafd2f4760fd8
Aristeides Stathopoulos <aristath@gmail.com>
2013-04-20 13:26:31 +0300

    better notices

--- lib/admin/notices.php
+++ lib/admin/notices.php
@@ -22,17 +22,23 @@ function shoestrap_check_files_permissions() {
       $notice = false;
     
     if ( $notice == true ) {
-      echo '<div>';
+      echo '<div class="error">';
+        echo '<h2>' . __( 'Important notice:', 'shoestrap' ) . '</h2>';
+        echo '<p>' . __( 'The advanced mode of the Shoestrap theme requires some files in your theme folder to be writable by your webserver.', 'shoestrap' ) . '</p>';
+        echo '<p>' . __( 'Please make sure that all files listed below are marked as Writable.', 'shoestrap' ) . '</p>';
+        echo '<p>' . __( 'In case you see a file marked as "Not Writable", you will have to change its permissions and make it writable.', 'shoestrap' ) . '</p>';
+        echo '<table class="table table-bordered" style="width: 100%;">';
       foreach ( $files as $file ) {
-        echo '<p>';
-        echo $file . ' : ';
+        echo '<tr><td style="border-bottom: 1px solid;">' . $file . ' : </td>';
         if ( is_writable( $file ) )
-          echo '<span style="color: #00aa00;">' .  __( 'Writable', 'shoestrap' );
+          echo '<td style="border-bottom: 1px solid;"><strong>' .  __( 'Writable', 'shoestrap' ) . '</strong></td>';
         else
-          echo '<span style="color: #aa0000;">' .  __( 'Not Writable', 'shoestrap' );
+          echo '<td style="border-bottom: 1px solid;"><strong>' .  __( 'Not Writable', 'shoestrap' ) . '</strong></td>';
         
-        echo '</p>';
+        echo '</tr>';
       }
+      echo '</table>';
+      echo '<p>' . __( 'Once you make your changes please refresh this page. Once you see no more error messages you\'ll be able to use the developer tools.', 'shoestrap' ) . '</p>';
       echo '</div>';
     }
   }
