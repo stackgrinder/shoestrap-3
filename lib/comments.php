1cbaa0c2058a738f49579f1d1af70c0f5e19bca1
Aristeides Stathopoulos <aristath@gmail.com>
2013-04-20 21:13:56 +0300

    add comment template https://github.com/retlehs/roots/commit/37e5423b77cfe5ffefa968a9fd9d8227a28da23b

--- lib/comments.php
+++ lib/comments.php
@@ -34,23 +34,8 @@ class Shoestrap_Walker_Comment extends Walker_Comment {
     }
 
     extract($args, EXTR_SKIP); ?>
-
-  <li id="comment-<?php comment_ID(); ?>" <?php comment_class( 'media comment-' . get_comment_ID() ); ?>>
-    <?php echo get_avatar($comment, $size = '64'); ?>
-    <div class="media-body">
-      <h4 class="media-heading"><?php echo get_comment_author_link(); ?></h4>
-      <time datetime="<?php echo comment_date('c'); ?>"><a href="<?php echo htmlspecialchars(get_comment_link($comment->comment_ID)); ?>"><?php printf(__('%1$s', 'shoestrap'), get_comment_date(),  get_comment_time()); ?></a></time>
-      <?php edit_comment_link(__('(Edit)', 'shoestrap'), '', ''); ?>
-
-      <?php if ($comment->comment_approved == '0') : ?>
-        <div class="alert alert-block fade in">
-          <a class="close" data-dismiss="alert">&times;</a>
-          <p><?php _e('Your comment is awaiting moderation.', 'shoestrap'); ?></p>
-        </div>
-      <?php endif; ?>
-
-      <?php comment_text(); ?>
-      <?php comment_reply_link(array_merge($args, array('depth' => $depth, 'max_depth' => $args['max_depth']))); ?>
+  <li <?php comment_class('media comment-' . get_comment_ID()); ?>>
+    <?php include(locate_template('templates/comment.php')); ?>
   <?php
   }
 
