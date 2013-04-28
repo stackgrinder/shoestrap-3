33b0707c98ed693761f604499cb1b318a5b4c89d
Aristeides Stathopoulos <aristath@gmail.com>
2013-04-28 12:02:27 +0300

    Adding help text for background color.

--- lib/customizer/background/functions.php
+++ lib/customizer/background/functions.php
@@ -10,9 +10,12 @@ function shoestrap_background_customizer( $wp_customize ){
   $background_color = '#' . str_replace( '#', '', $background_color );
   set_theme_mod( 'background_color', get_theme_mod( 'shoestrap_background_color' ) );
   
+  $background_color_help = __( 'You can use the below color control to select a background color for your page', 'shoestrap' );
+
   $settings   = array();
   // Color Settings
   $settings[] = array( 'slug' => 'shoestrap_background_color',          'default' => $background_color );
+  $settings[] = array( 'slug' => 'shoestrap_background_color_help',     'default' => $background_color_help );
 
   foreach( $settings as $setting ){
     $wp_customize->add_setting( $setting['slug'], array( 'default' => $setting['default'], 'type' => 'theme_mod', 'capability' => 'edit_theme_options' ) );
@@ -27,7 +30,7 @@ function shoestrap_background_customizer( $wp_customize ){
   $color_controls   = array();
   
   // Background Color
-  $color_controls[] = array( 'setting' => 'shoestrap_background_color', 'label' => 'Background Color', 'section' => 'colors', 'priority' => 1 );
+  $color_controls[] = array( 'setting' => 'shoestrap_background_color', 'label' => 'Background Color', 'section' => 'colors', 'priority' => 2 );
   
   foreach( $color_controls as $control ){
     $wp_customize->add_control( new WP_Customize_Color_Control(
@@ -41,5 +44,13 @@ function shoestrap_background_customizer( $wp_customize ){
       )
     ));
   }
+
+  // Background color help text
+  $wp_customize->add_control( new Shoestrap_Customize_Label_Control( $wp_customize, 'shoestrap_background_color_help', array(
+    'label'       => $background_color_help,
+    'section'     => 'colors',
+    'settings'    => 'shoestrap_background_color_help',
+    'priority'    => 1,
+  )));
 }
 add_action( 'customize_register', 'shoestrap_background_customizer' );
