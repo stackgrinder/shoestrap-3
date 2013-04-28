ea64a1babad342aad10ee4dbfad2f17ee26155fe
Aristeides Stathopoulos <aristath@gmail.com>
2013-04-29 02:11:47 +0300

    moving content template to cleanup.php and using an action to call it

--- templates/content.php
+++ templates/content.php
@@ -1,14 +1,1 @@
-<article <?php post_class(); ?>>
-  <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php get_template_part('templates/entry-meta'); ?>
-  </header>
-  <div class="entry-summary">
-    <?php do_action( 'shoestrap_entry_summary_begin' ); ?>
-    <?php do_action( 'shoestrap_the_excerpt' ); ?>
-    <?php do_action( 'shoestrap_entry_summary_end' ); ?>
-  </div>
-  <footer>
-    <?do_action( 'shoestrap_post_footer' ); ?>
-  </footer>
-</article>
+<?php do_action( 'shoestrap_article_content' ); ?>
