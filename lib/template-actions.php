e1ca36dff27e5025af0acaeddf3f0deafee17ec8
Aristeides Stathopoulos <aristath@gmail.com>
2013-04-30 16:52:19 +0300

    adding more hooks for templates

--- lib/template-actions.php
+++ lib/template-actions.php
@@ -63,23 +63,38 @@ add_action( 'shoestrap_do_metadata', 'shoestrap_article_metadata', 10 );
 /*
  * The single-post content template
  */
-function shoestrap_single_content() { ?>
-  <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-    <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part( 'templates/entry-meta' ); ?>
-    </header>
-    <div class="entry-content">
-      <?php do_action( 'shoestrap_before_the_content' ); ?>
-      <?php the_content(); ?>
-      <?php do_action( 'shoestrap_after_the_content' ); ?>
-    </div>
-    <footer>
-      <?php wp_link_pages( array( 'before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'shoestrap' ), 'after' => '</p></nav>' ) ); ?>
-      <?php the_tags( '<i class="icon-tags"></i>',', ','' ); ?>
-    </footer>
-    <?php comments_template( '/templates/comments.php' ); ?>
-  </article>
+function shoestrap_single_content_template() { ?>
+  <?php while ( have_posts() ) : the_post(); ?>
+    <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+      <header>
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+        <?php get_template_part( 'templates/entry-meta' ); ?>
+      </header>
+      <div class="entry-content">
+        <?php do_action( 'shoestrap_before_the_content' ); ?>
+        <?php the_content(); ?>
+        <?php do_action( 'shoestrap_after_the_content' ); ?>
+      </div>
+      <footer>
+        <?php wp_link_pages( array( 'before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'shoestrap' ), 'after' => '</p></nav>' ) ); ?>
+        <?php the_tags( '<i class="icon-tags"></i>',', ','' ); ?>
+      </footer>
+      <?php comments_template( '/templates/comments.php' ); ?>
+    </article>
+  <?php endwhile; ?>
   <?php
 }
-add_action( 'shoestrap_single_content', 'shoestrap_single_content', 10 );
+add_action( 'shoestrap_single_content_template', 'shoestrap_single_content', 10 );
+
+/*
+ * The single-post content template
+ */
+function shoestrap_single_page_content_template() { ?>
+  <?php while ( have_posts() ) : the_post(); ?>
+    <?php do_action( 'shoestrap_before_the_content' ); ?>
+    <?php the_content(); ?>
+    <?php do_action( 'shoestrap_after_the_content' ); ?>
+    <?php wp_link_pages( array( 'before' => '<nav class="pagination">', 'after' => '</nav>' ) ); ?>
+  <?php endwhile;
+}
+add_action( 'shoestrap_single_page_content', 'shoestrap_single_page_content_template', 10 );
--- templates/content-single.php
+++ templates/content-single.php
@@ -1,3 +1,1 @@
-<?php while ( have_posts() ) : the_post(); ?>
-  <?php do_action( 'shoestrap_single_content' ); ?>
-<?php endwhile;
+<?php do_action( 'shoestrap_single_content' ); ?>
