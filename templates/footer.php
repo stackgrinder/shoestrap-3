ef816ddd135451026f42b0dcc53ff485b9939ca3
Aristeides Stathopoulos <aristath@gmail.com>
2013-07-11 00:24:35 +0300

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -19,5 +19,4 @@
 <?php endif; ?>
 
 <?php wp_footer(); ?>
-<?php if ( get_option( 'shoestrap_load_scripts_on_footer' ) == 1 && is_single() && comments_open() && get_option( 'thread_comments' ) ) wp_print_scripts( 'comment-reply' ); ?>
-<?php do_action( 'shoestrap_after_footer' );
+<?php if ( get_option( 'shoestrap_load_scripts_on_footer' ) == 1 && is_single() && comments_open() && get_option( 'thread_comments' ) ) wp_print_scripts( 'comment-reply' );
